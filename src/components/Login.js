b51d5cd4c41bff2821ba68a3aa481bf166c5e71a
santoshkumarm1 <santoshkumarm1@verifone.com>
2025-07-21 11:22:47 +0530

    basic login done.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,31 @@
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    localStorage.setItem("token", "dummy-token");
+  const handleLogin = async () => {
+    try {
+      // Integration with backend for login
+      const url = "http://localhost:8080/auth/login";
+      const payload = { username: "cdac123", password: "cdac" };
+      const response = await axios.post(url, payload);
 
-    // Redirect to the following page after login
-    // navigate("/following", { replace: true });
-    window.location.href = "/following";
+      if (response.status === 200) {
+        // localStorage.setItem("token", "dummy-token");
+        localStorage.setItem("token", response.data.access_token);
+
+        // Redirect to the following page after login
+        // navigate("/following", { replace: true });
+        window.location.href = "/following";
+      } else {
+        alert("Login failed");
+        return;
+      }
+    } catch (err) {
+      alert("Login failed");
+      return;
+    }
   };
 
   return (
